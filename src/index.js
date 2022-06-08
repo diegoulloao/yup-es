04fef4c87144ca02e1670f0e263b2d87fe7d615a
Jeremias Araujo <jerearaujo03@gmail.com>
2022-06-08 20:02:01 -0300

    replace es6 with commonjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /**
- * 
+ *
  *  Index: yup-es
  *  @description point of import
- * 
+ *
  *  @author diegoulloao
- * 
+ *
  */
-import * as es from './lib/es'
-export default es
+const es = require("./lib/es");
+module.export = es;
