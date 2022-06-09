32c53de254b44d8afd24c933d06f012333b95385
Diego <diegoulloao@icloud.com>
2022-06-08 21:22:45 -0400

    fix: types (beta)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /**
  *
- *  Index: yup-es
- *  @description point of import
+ *  Yup-es
+ *  @description import entry
  *
  *  @author diegoulloao
  *
  */
-const es = require("./lib/es");
-module.export = es;
+
+const es = require("./lib/es")
+module.export = es
--- src/lib/es.js
+++ src/lib/es.js
@@ -1,27 +1,32 @@
-/*eslint-disable no-template-curly-in-string*/
-const printValue = require("yup/lib/util/printValue");
 // Based on https://github.com/jquense/yup/blob/2973d0a/src/locale.js
+
+const printValue = require("yup/lib/util/printValue")
+
 const mixed = {
   default: "${path} no es válido.",
   required: "${path} es un campo obligatorio",
   oneOf: "${path} debe ser uno de los siguientes valores: ${values}",
   notOneOf: "${path} no debe ser uno de los siguientes valores: ${values}",
   notType: ({ path, type, value, originalValue }) => {
-    const isCast = originalValue != null && originalValue !== value;
-    let msg =
+    const isCast = originalValue != null && originalValue !== value
+
+    let msg = (
       `${path} debe ser un \`${type}\` Tipo, ` +
       `pero el valor final fue: \`${printValue(value, true)}\`` +
-      (isCast
-        ? ` (Reparto del valor \`${printValue(originalValue, true)}\`).`
-        : ".");
+      (isCast ? ` (Reparto del valor \`${printValue(originalValue, true)}\`).` : ".")
+    )
+
     if (value === null) {
-      msg +=
+      msg += (
         `\n Si "nulo" pretende ser un valor vacío, asegúrese de marcar el esquema como` +
-        " `.nullable()`";
+        " `.nullable()`"
+      )
     }
-    return msg;
-  },
-};
+
+    return msg
+  }
+}
+
 const string = {
   length: "${path} debe ser exactamente ${length} caracteres",
   min: "${path} debe ser de al menos ${min} caracteres",
@@ -31,8 +36,9 @@ const string = {
   url: "${path} debe ser una URL válida",
   trim: "${path} debe ser una cadena recortada",
   lowercase: "${path} debe ser una cadena en minúsculas",
-  uppercase: "${path} debe ser una cadena en mayúsculas",
-};
+  uppercase: "${path} debe ser una cadena en mayúsculas"
+}
+
 const number = {
   min: "${path} debe ser mayor que o igual a ${min}",
   max: "${path} debe ser menor que o igual a ${max}",
@@ -40,21 +46,24 @@ const number = {
   moreThan: "${path} debe ser mayor que ${more}",
   positive: "${path} debe ser un número positivo",
   negative: "${path} debe ser un número negativo",
-  integer: "${path} debe ser un entero",
-};
+  integer: "${path} debe ser un entero"
+}
+
 const date = {
   min: "${path} campo debe ser posterior a ${min}",
-  max: "${path} campo debe ser anterior a ${max}",
-};
-const boolean = {};
+  max: "${path} campo debe ser anterior a ${max}"
+}
+
+const boolean = {}
+
 const object = {
-  noUnknown:
-    "${path} campo no puede tener llaves no especificados en la forma del objeto",
-};
+  noUnknown: "${path} campo no puede tener llaves no especificados en la forma del objeto"
+}
+
 const array = {
   min: "${path} campo debe tener al menos ${min} elementos",
-  max: "${path} campo debe tener menos de o igual a ${max} elementos",
-};
+  max: "${path} campo debe tener menos de o igual a ${max} elementos"
+}
 
 module.export = {
   mixed,
@@ -63,5 +72,5 @@ module.export = {
   date,
   boolean,
   object,
-  array,
-};
+  array
+}
